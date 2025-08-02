5be3f542b14c0b4a9350f7e6d51927db6000e25b
forger6969 <saidazim186@gmail.com>
2025-08-02 21:14:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+
+let vopros = prompt('Сосал?')
+
+let vopros2 = 'да'
+
+let vopros3 = 'нет'
+
+if (vopros == vopros2) {
+
+    let otvet = alert('Xa-Xa-Xa pider')
+
+}
+
+if (vopros == vopros3) {
+
+
+    let otvet2 = alert('Уффф кефтеме ТИГР!!')
+
+}
+
+
+
+function myFunction() {
+
+
+    let vopros = prompt('Сосал?')
+
+    let vopros2 = 'да'
+
+    let vopros3 = 'нет'
+
+    if (vopros == vopros2) {
+
+        let otvet = alert('Xa-Xa-Xa pider')
+
+    }
+
+    if (vopros == vopros3) {
+
+
+        let otvet2 = alert('Уффф кефтеме ТИГР!!')
+
+    }
+
+
+}
